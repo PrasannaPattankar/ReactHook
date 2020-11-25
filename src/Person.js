0b1d2f3b222de806b733f835a6249098868e7714
PrasannaPattankar <prasanna.pattankar@gmail.com>
2020-11-25 23:00:03 +0530

    work

---
+++ src/Person.js
@@ -0,0 +1,25 @@
+import React from 'react'
+// const person = (props) =>{
+//     return(
+//         <div>
+// <p>i am {props.name} and my age is{props.age}</p>
+//     <p>{props.children}</p>
+//         </div>
+
+        
+
+//     )
+// }
+
+const person = (props) =>{
+    return(
+        <div>
+           <p>My name is {props.name} and my age is{props.age} location is {props.Location}</p>
+
+        </div>
+    )
+
+}
+
+
+export default person;
